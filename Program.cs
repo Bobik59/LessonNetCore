1a0017bd3185e74bdd188660504a36881e16b2c1
Bobik58 <loshininc@gmail.com>
2025-02-26 18:18:05 +0700

    save

--- Program.cs
+++ Program.cs
@@ -1,6 +1,363 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿////������� ����������
+//void Example1()
+//{
+//    var builder = WebApplication.CreateBuilder(args);
+//    var app = builder.Build();
+//    //��������
+//    //Configuration: ������������ ������������ ���������� � ���� ������� IConfiguration
+//    //Environment: ������������ ��������� ���������� � ���� IWebHostEnvironment
+//    //Lifetime: ��������� �������� ����������� � �������� ���������� ����� ����������
+//    //Logger: ������������ ������ ���������� �� ���������
+//    //Services: ������������ ������� ����������
+//    //Urls: ������������ ����� �������, ������� ���������� ������
 
-app.MapGet("/", () => "Hello World!");
+//    app.MapGet("/", () => "Hello World!");
+//    //���������� middleware ������������ � ������� ������� ���������� Run, Map � Use ���������� IApplicationBuilder
+//    //app.UseWelcomePage();
 
-app.Run();
+//    app.Run();
+//}
+
+////����� Run � ������ HttpContext
+//void Example2()
+//{
+//    //�������� middleware
+//    //public delegate Task RequestDelegate(HttpContext context);
+//    //�������� HttpContext
+//    //Connection: ������������ ���������� � �����������, ������� ����������� ��� ������� �������
+//    //Features: �������� ��������� HTTP-�����������������, ������� �������� ��� ����� �������
+//    //Items: �������� ��� ������������� ��������� ��� ����-�������� ��� �������� ��������� ������ ��� �������� �������
+//    //Request: ���������� ������ HttpRequest, ������� ������ ���������� � ������� �������
+//    //RequestAborted: ���������� ����������, ����� ����������� �����������, � �������������� ��������� ������� ������ ���� ��������
+//    //RequestServices: �������� ��� ������������� ������ IServiceProvider, ������� ������������� ������ � ���������� �������� �������
+//    //Response: ���������� ������ HttpResponse, ������� ��������� ��������� ������� �������
+//    //Session: ������ ������ ������ ��� �������� �������
+//    //TraceIdentifier: ������������ ���������� ������������� ������� ��� ����� �����������
+//    //User: ������������ ������������, ���������������� � ���� ��������
+//    //WebSockets: ���������� ������ ��� ���������� ������������� WebSocket ��� ������� �������
+
+//    var builder = WebApplication.CreateBuilder();
+
+//    var app = builder.Build();
+
+//    app.Run(async (context) => await context.Response.WriteAsync(DateTime.Now.ToLongDateString()));
+//    //app.Run(HandleRequst);
+
+//    app.Run();
+
+//    async Task HandleRequst(HttpContext context)
+//    {
+//        await context.Response.WriteAsync(DateTime.Now.ToLongDateString());
+//    }
+//}
+
+////Response
+//void Example3()
+//{
+//    //�������� Response
+
+//    //Body: �������� ��� ������������� ���� ������ � ���� ������� Stream
+//    //BodyWriter: ���������� ������ ���� PipeWriter ��� ������ ������
+//    //ContentLength: �������� ��� ������������� ��������� Content-Length
+//    //ContentType: �������� ��� ������������� ��������� Content-Type
+//    //Cookies: ���������� ����, ������������ � ������
+//    //HasStarted: ���������� true, ���� �������� ������ ��� ��������
+//    //Headers: ���������� ��������� ������
+//    //Host: �������� ��� ������������� ��������� Host
+//    //HttpContext: ���������� ������ HttpContext, ��������� � ������ �������� Response
+//    //StatusCode: ���������� ��� ������������� ��������� ��� ������
+
+//    var builder = WebApplication.CreateBuilder();
+//    var app = builder.Build();
+
+//    app.Run(HandleRequst);
+//    //app.Run(HandleRequstHtml);
+
+//    app.Run();
+
+//    async Task HandleRequst(HttpContext context)
+//    {
+//        var response = context.Response;
+//        response.Headers.ContentLanguage = "ru-RU";
+//        response.Headers.ContentType = "text/plain; charset=utf-8";
+
+//        response.StatusCode = 200;
+
+//        await response.WriteAsync(DateTime.Now.ToLongDateString());
+//    }
+//    //�������� html
+//    async Task HandleRequstHtml(HttpContext context)
+//    {
+//        var response = context.Response;
+//        response.ContentType = "text/html; charset=utf-8";
+
+//        await response.WriteAsync("<h1>Hello world!</h1>");
+//    }
+//}
+
+////Request
+//void Example4()
+//{
+//    //�������� Request
+//    //Body: ������������� ���� ������� � ���� ������� Stream
+//    //BodyReader: ���������� ������ ���� PipeReader ��� ������ ���� �������
+//    //ContentLength: �������� ��� ������������� ��������� Content-Length
+//    //ContentType: �������� ��� ������������� ��������� Content-Type
+//    //Cookies: ���������� ��������� ���� (������ Cookies), ��������������� � ������ ��������
+//    //Form: �������� ��� ������������� ���� ������� � ���� ����
+//    //HasFormContentType: ��������� ������� ��������� Content-Type
+//    //Headers: ���������� ��������� �������
+//    //Host: �������� ��� ������������� ��������� Host
+//    //HttpContext: ���������� ��������� � ������ �������� ������ HttpContext
+//    //IsHttps: ���������� true, ���� ����������� �������� https
+//    //Method: �������� ��� ������������� ����� HTTP
+//    //Path: �������� ��� ������������� ���� ������� � ���� ������� RequestPath
+//    //PathBase: �������� ��� ������������� ������� ���� �������. ����� ���� �� ������ ��������� ����������� ����
+//    //Protocol: �������� ��� ������������� ��������, ��������, HTTP
+//    //Query: ���������� ��������� ���������� �� ������ �������
+//    //QueryString: �������� ��� ������������� ������ �������
+//    //RouteValues: �������� ������ �������� ��� �������� �������
+//    //Scheme: �������� ��� ������������� ����� ������� HTTP
+
+//    var builder = WebApplication.CreateBuilder();
+//    var app = builder.Build();
+//    app.Run(HandleRequstHeaders);
+//    app.Run();
+
+//    async Task HandleRequstHeaders(HttpContext context)
+//    {
+//        context.Response.ContentType = "text/html; charset=utf-8";
+//        var stringBuilder = new System.Text.StringBuilder("<table>");
+
+//        foreach (var header in context.Request.Headers)
+//        {
+//            stringBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+//        }
+//        stringBuilder.Append("</table>");
+//        await context.Response.WriteAsync(stringBuilder.ToString());
+//    }
+//    async Task HandleRequstPath(HttpContext context)
+//    {
+//        var path = context.Request.Path;
+//        var response = context.Response;
+
+//        if (path == "/date")
+//            await response.WriteAsync($"Date: {DateTime.Now.ToShortDateString()}");
+//        else if (path == "/time")
+//            await response.WriteAsync($"Time: {DateTime.Now.ToShortTimeString()}");
+//        else
+//            await response.WriteAsync("�������� �� �������.");
+//    }
+//    //������ �������
+//}
+
+////��������� �����
+void Example5()
+{
+    var builder = WebApplication.CreateBuilder();
+    var app = builder.Build();
+
+    app.Run(HandleRequst);
+    app.Run();
+
+    async Task HandleRequst(HttpContext context)
+    {
+        context.Response.ContentType = "text/html; charset=utf-8";
+
+        if (context.Request.Path == "/postuser")
+        {
+            var form = context.Request.Form;
+            string name = form["name"];
+            string age = form["age"];
+            await context.Response.WriteAsync($"<div><p>Name: {name}</p><p>Age: {age}</p></div>");
+        }
+        else
+        {
+            await context.Response.SendFileAsync("html/Example5.html");
+        }
+    }
+}
+
+////������ � Json
+////void Example6()
+////{
+////    var builder = WebApplication.CreateBuilder();
+////    var app = builder.Build();
+
+////    app.Run(HandleRequst);
+////    app.Run();
+
+////    async Task HandleRequst(HttpContext context)
+////    {
+////        var response = context.Response;
+////        var request = context.Request;
+////        if (request.Path == "/api/user")
+////        {
+////            var message = "������������ ������";   // ���������� ��������� �� ���������
+////            try
+////            {
+////                // �������� �������� ������ json
+////                var person = await request.ReadFromJsonAsync<Person>();
+////                if (person != null) // ���� ������ ��������������� � Person
+////                    message = $"Name: {person.Name}  Age: {person.Age}";
+////            }
+////            catch
+////            {
+////                app.Logger.LogError("������ ������ ������.");
+////            }
+////            // ���������� ������������ ������
+////            await response.WriteAsJsonAsync(new { text = message });
+////        }
+////        else
+////        {
+////            response.ContentType = "text/html; charset=utf-8";
+////            await response.SendFileAsync("html/Example6.html");
+////        }
+////    }
+////}
+
+////����� Use
+//void Example7()
+//{
+//    var builder = WebApplication.CreateBuilder();
+//    var app = builder.Build();
+
+//    string date = "";
+
+//    app.Use(async (context, next) =>
+//    {
+//        date = DateTime.Now.ToShortDateString();
+//        await next.Invoke();
+//        Console.WriteLine($"Current date: {date}");
+//    });
+//    //app.Use(async (context, next) =>
+//    //{
+//    //    date = DateTime.Now.ToShortDateString();
+//    //    await next.Invoke(context);                 // ����� next - RequestDelegate
+//    //    Console.WriteLine($"Current date: {date}");
+//    //});
+
+//    app.Run(async (context) => await context.Response.WriteAsync($"Date: {date}"));
+//    app.Run();
+
+
+//    async Task GetDateFunc(HttpContext context, Func<Task> next)
+//    {
+//        date = DateTime.Now.ToShortDateString();
+//        await next.Invoke();
+//        Console.WriteLine($"Current date: {date}");
+//    }
+//    async Task GetDateRequestDelegate(HttpContext context, RequestDelegate next)
+//    {
+//        date = DateTime.Now.ToShortDateString();
+//        await next.Invoke(context);
+//        Console.WriteLine($"Current date: {date}");
+//    }
+//    async Task GetDateRequestTerminal(HttpContext context, RequestDelegate next)
+//    {
+//        string path = context.Request.Path;
+//        if (path == "/date")
+//        {
+//            await context.Response.WriteAsync($"Date: {DateTime.Now.ToShortDateString()}");
+//        }
+//        else
+//        {
+//            await next.Invoke(context);
+//        }
+//    }
+//}
+
+////����� UseWhen
+//void Example8()
+//{
+//    //public static IApplicationBuilder UseWhen (this IApplicationBuilder app, Func<HttpContext,bool> predicate,
+//    //  Action<IApplicationBuilder> configuration);
+//    //������� Func<HttpContext,bool> - ��������� �������, �������� ������ ��������������� ������
+//    //������� Action<IApplicationBuilder> ������������ ��������� �������� ��� �������� IApplicationBuilder
+
+//    var builder = WebApplication.CreateBuilder();
+//    var app = builder.Build();
+
+//    app.UseWhen(
+//        context => context.Request.Path == "/time", // ���� ���� ������� "/time"
+//        appBuilder =>
+//        {
+//            string time = null;
+//            // ��������� ������ - ������� �� ������� ����������
+//            appBuilder.Use(async (context, next) =>
+//            {
+//                time = DateTime.Now.ToShortTimeString();
+//                Console.WriteLine($"Time: {time}");
+//                await next();   // �������� ��������� middleware
+//            });
+
+//            // ���������� �����
+//            appBuilder.Run(async context =>
+//            {
+//                await context.Response.WriteAsync($"Time: {time}");
+//            });
+//        });
+
+//    app.Run(async context =>
+//    {
+//        await context.Response.WriteAsync("Home page");
+//    });
+
+//    app.Run();
+//}
+////����� Map
+//void Example9()
+//{
+//    var builder = WebApplication.CreateBuilder();
+//    var app = builder.Build();
+
+//    app.Map("/time", appBuilder =>
+//    {
+//        var time = DateTime.Now.ToShortTimeString();
+
+//        appBuilder.Use(async (context, next) =>
+//        {
+//            Console.WriteLine($"Time: {time}");
+//            await next();
+//        });
+
+//        appBuilder.Run(async context => await context.Response.WriteAsync($"Time: {time}"));
+//    });
+
+//    app.Run(async (context) => await context.Response.WriteAsync("Hello world!"));
+
+//    app.Run();
+//}
+
+////��������� Map
+//void Example10()
+//{
+//    var builder = WebApplication.CreateBuilder();
+//    var app = builder.Build();
+
+//    app.Map("/home", appBuilder =>
+//    {
+//        appBuilder.Map("/index", Index);
+//        appBuilder.Map("/about", About);
+
+//        appBuilder.Run(async (context) => await context.Response.WriteAsync("Home Page"));
+//    });
+
+//    app.Run(async (context) => await context.Response.WriteAsync("Page Not Found"));
+
+//    app.Run();
+
+//    void Index(IApplicationBuilder appBuilder)
+//    {
+//        appBuilder.Run(async context => await context.Response.WriteAsync("Index Page"));
+//    }
+//    void About(IApplicationBuilder appBuilder)
+//    {
+//        appBuilder.Run(async context => await context.Response.WriteAsync("About Page"));
+//    }
+//}
+
+
+
+//public record Person(string Name, int Age);
+
---
+++ theory.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+void Example3()
+{
+    var builder = WebApplication.CreateBuilder();
+    var app = builder.Build();
+
+    var books = new List<Book>
+        {
+            new Book { Id = 1, Title = "1984", Author = "George Orwell", Year = 1949, Genre = "Dystopian, Political fiction", Isbn = "978-0451524935" },
+            new Book { Id = 2, Title = "Brave New World", Author = "Aldous Huxley", Year = 1932, Genre = "Dystopian, Science fiction", Isbn = "978-0060850524" },
+            new Book { Id = 3, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960, Genre = "Southern Gothic, Drama", Isbn = "978-0061120084" },
+            new Book { Id = 4, Title = "The Catcher in the Rye", Author = "J.D. Salinger", Year = 1951, Genre = "Fiction, Coming-of-age", Isbn = "978-0316769488" },
+            new Book { Id = 5, Title = "Pride and Prejudice", Author = "Jane Austen", Year = 1813, Genre = "Romance, Satire", Isbn = "978-1503290563" },
+            new Book { Id = 6, Title = "Moby-Dick", Author = "Herman Melville", Year = 1851, Genre = "Adventure, Epic", Isbn = "978-1503280786" },
+            new Book { Id = 7, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Year = 1925, Genre = "Tragedy, Novel", Isbn = "978-0743273565" },
+            new Book { Id = 8, Title = "The Hobbit", Author = "J.R.R. Tolkien", Year = 1937, Genre = "Fantasy, Adventure", Isbn = "978-0618968633" },
+            new Book { Id = 9, Title = "War and Peace", Author = "Leo Tolstoy", Year = 1869, Genre = "Historical fiction", Isbn = "978-1400079988" },
+            new Book { Id = 10, Title = "The Brothers Karamazov", Author = "Fyodor Dostoevsky", Year = 1880, Genre = "Philosophical, Fiction", Isbn = "978-0374528379" },
+            new Book { Id = 11, Title = "Crime and Punishment", Author = "Fyodor Dostoevsky", Year = 1866, Genre = "Psychological fiction", Isbn = "978-0486415871" },
+            new Book { Id = 12, Title = "Les Misérables", Author = "Victor Hugo", Year = 1862, Genre = "Historical fiction", Isbn = "978-0451419439" },
+            new Book { Id = 13, Title = "The Picture of Dorian Gray", Author = "Oscar Wilde", Year = 1890, Genre = "Philosophical fiction, Gothic", Isbn = "978-0486266846" },
+            new Book { Id = 14, Title = "The Odyssey", Author = "Homer", Year = 8, Genre = "Epic, Adventure", Isbn = "978-0140268867" },
+            new Book { Id = 15, Title = "Dracula", Author = "Bram Stoker", Year = 1897, Genre = "Gothic horror", Isbn = "978-0486411095" },
+            new Book { Id = 16, Title = "Frankenstein", Author = "Mary Shelley", Year = 1818, Genre = "Gothic horror, Science fiction", Isbn = "978-0486282112" },
+            new Book { Id = 17, Title = "Alice's Adventures in Wonderland", Author = "Lewis Carroll", Year = 1865, Genre = "Fantasy, Adventure", Isbn = "978-0486275435" },
+            new Book { Id = 18, Title = "The Lord of the Rings", Author = "J.R.R. Tolkien", Year = 1954, Genre = "Fantasy, Adventure", Isbn = "978-0618640157" },
+            new Book { Id = 19, Title = "Fahrenheit 451", Author = "Ray Bradbury", Year = 1953, Genre = "Dystopian, Science fiction", Isbn = "978-1451673319" },
+            new Book { Id = 20, Title = "The Catcher in the Rye", Author = "J.D. Salinger", Year = 1951, Genre = "Fiction, Coming-of-age", Isbn = "978-0316769488" }
+        };
+
+    app.Map("/home", appBuilder =>
+    {
+        appBuilder.Map("/books", Books);
+
+
+        appBuilder.Run(async (context) => await context.Response.WriteAsync("Home Page"));
+    });
+
+    app.Run();
+
+    void Books(IApplicationBuilder appBuilder)
+    {
+        appBuilder.Run(async (context) => await context.Response.SendFileAsync("index.html"));
+    }
+}
+
+//    void Book(IApplicationBuilder appBuilder)
+//    {
+//        appBuilder.UseWhen(
+//            (context) =>
+//            {
+//                return int.TryParse(context.Request.Path.ToString().Split("/").Last(), out _);
+//            },
+//            (app) =>
+//            {
+//                int? ID = null;
+//                appBuilder.Use(async (context, next) =>
+//                {
+//                    ID = Convert.ToInt32(context.Request.Path.ToString().Split("/").Last());
+
+//                    var book = books.Find(b => b.Id == ID);
+//                    if (book != null)
+//                    {
+                        
+//                    }
+//                    else
+//                    {
+//                        context.Response.StatusCode = 404;
+//                        await context.Response.WriteAsync("<html><body><h1>Book not found</h1></body></html>");
+//                    }
+//                    await next();
+//                });
+//            }
+//        );
+//    }
+
+//}
+Example3();
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public string Genre { get; set; }
+    public string Isbn { get; set; }
+}
