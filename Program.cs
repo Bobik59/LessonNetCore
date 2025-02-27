63062fdfe8c416883a1bc4a6c7b426b938113d9d
Bobik58 <loshininc@gmail.com>
2025-02-27 16:32:39 +0700

    save

--- Program.cs
+++ Program.cs
@@ -41,4 +41,29 @@ app.Map("/api", (appBulder) =>
     });
 });
 
+app.UseWhen(
+            (context) =>
+            {
+                return int.TryParse(context.Request.Path.ToString().Split("/").Last(), out _);
+            },
+            (app) =>
+            {
+                int? ID = null;
+                app.Use(async (context, next) =>
+                {
+                    ID = Convert.ToInt32(context.Request.Path.ToString().Split("/").Last());
+
+                    var book = books.Find(b => b.Id == ID);
+                    if (book != null)
+                    {
+                        app.Run(async (context) =>
+                        {
+                            await context.Response.WriteAsJsonAsync(book);
+                        });
+                    }
+                    await next();
+                });
+            }
+        );
+
 app.Run();
