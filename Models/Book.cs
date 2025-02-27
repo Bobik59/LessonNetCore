5668ddcfa77a1932416b5085cc1636b1aba7d6ce
Bobik58 <loshininc@gmail.com>
2025-02-27 15:45:17 +0700

    save

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace LessonNetCore.Models
+{
+    public class Book
+    {
+        public static int Count { get; set; }
+        static Book()
+        {
+            Count = 1;
+        }
+
+        public Book()
+        {
+            Id = Count++;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+    }
+}
