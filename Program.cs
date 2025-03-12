7fdfe12166134fcd95d30fd18d90d2cb17475a38
Bobik58 <loshininc@gmail.com>
2025-03-12 17:35:14 +0700

    WorkDb

--- Program.cs
+++ Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using DataAccess;
+using DataAccess.Models;
+using DataAccess.Repository.Abstract;
+using DataAccess.Repository.EntityFramework;
+using LessonNetCore.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder();
+
+// ��������� ���� ������
+var connString =
+    @"Server=(localdb)\MSSQLLocalDB; Database=StudentDb; Trusted_Connection=True;";
+builder.Services.AddDbContext<DbContextStudent>(options
+    => options.UseSqlServer(connString));
+builder.Services.AddScoped<IStudentRepository, EntityFrameworkStudent>();
+
+builder.Services.AddScoped<RequestHandlerService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/students",
+    async (RequestHandlerService requestHandler) =>
+    await requestHandler.GetAllStudentsAsync());
+app.MapGet("/student/{id:int}",
+    async (int id, RequestHandlerService requestHandler) =>
+    await requestHandler.GetStudentByIdAsync(id));
+app.MapPost("/student/create",
+    async (IFormCollection form, RequestHandlerService requestHandler) =>
+    await requestHandler.CreateStudentAsync(form)).DisableAntiforgery();
 
 app.Run();
---
+++ Services/RequestHandlerService.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Models;
+using DataAccess.Repository.Abstract;
+
+namespace LessonNetCore.Services
+{
+    public class RequestHandlerService
+    {
+        private IStudentRepository _studentRepository;
+        public RequestHandlerService(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<IResult> GetAllStudentsAsync()
+        {
+            var students = await _studentRepository.GetAll();
+            return Results.Json(students);
+        }
+        public async Task<IResult> GetStudentByIdAsync(int id)
+        {
+            var student = await _studentRepository.GetById(id);
+
+            if (student == null)
+                return Results.NotFound();
+
+            return Results.Json(student);
+        }
+        public async Task<IResult> CreateStudentAsync(IFormCollection form)
+        {
+            var student = new Student()
+            {
+                Name = form["name"],
+                Surname = form["surname"]
+            };
+
+            var id = await _studentRepository.Create(student);
+            return Results.Ok(id);
+        }
+    }
+}
